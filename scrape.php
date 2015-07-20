89594ddb5d855699f29bf5610970409902da035f
Daniel <dusoft@staznosti.sk>
2015-07-20 21:45:56 +0200

    updated for template generation and gdrive upload

--- scrape.php
+++ scrape.php
@@ -9,6 +9,24 @@ require('config.php');
 require('simple_html_dom.php');
 require('PHPMailer/PHPMailerAutoload.php');
 $hashes=file_get_contents('lasthash.txt');
+if ($config["gdrive-clientid"])
+   {
+   define('GOOGLE_CLIENT_ID',$config['gdrive-clientid']);
+   define('GOOGLE_CLIENT_EMAIL',$config['gdrive-clientemail']);
+   define('GOOGLE_APPLICATION_SCOPE','https://www.googleapis.com/auth/drive');
+   define('GOOGLE_APPLICATION_NAME','EIA Scraper');
+   define('GOOGLE_KEY_FILE',$config['gdrive-keyfile']);
+   require("vendor/autoload.php");
+   require("helpers.class.php");
+   $service=new Google_Service_Drive(getClient());
+   $files=$service->files;
+   }
+if ($config["template"])
+   {
+   require_once 'PHPWord/src/PhpWord/Autoloader.php';
+   \PhpOffice\PhpWord\Autoloader::register();
+   $addresses=file($config["addresses"]);
+   }
 
 $mail=new PHPMailer;
 $mail->isSMTP();                                      // Set mailer to use SMTP
@@ -22,12 +40,11 @@ $mail->CharSet = 'UTF-8';
 $mail->From = $config["from"];
 $mail->FromName = $config["fromname"];
 $mail->addAddress($config["to"]);     // Add a recipient
-
 $mail->Subject = $config["subject"];
 
 $filtercity=$config["filter"];
 
-$html=file_get_html("http://www.enviroportal.sk/sk/eia/print?search[name]=&search[act]=&search[activity]=&search[country]=1&search[district]=101&search[state]=&search[crossborder_country]=&search[_token]=3e7c06fc2c9fd18ea7e969d387da10050707bdc6");
+$html=file_get_html("http://www.enviroportal.sk/sk/eia/print");
 
 $i=0;
 foreach($html->find('tr') as $line)
@@ -46,6 +63,10 @@ foreach($html->find('tr') as $line)
       $projects[$i]["city"]=trim(preg_replace('!\s+!', ' ', $city));
       $projects[$i]["category"]=trim($category);
       $projects[$i]["status"]=trim($status);
+      $htmlchild=file_get_html('http://www.enviroportal.sk'.$link);
+      $projects[$i]["institution"]=trim($htmlchild->find('.table-list li',2)->getElementByTagName('span')->plaintext);
+      $projects[$i]["proponent"]=trim($htmlchild->find('.table-list li',3)->getElementByTagName('span')->plaintext);
+      $projects[$i]["proponentidnumber"]=trim($htmlchild->find('.table-list li',4)->getElementByTagName('span')->plaintext);
       $i++;
       }
    }
@@ -59,21 +80,88 @@ foreach($projects as $project)
    if (stripos($hashes,$hash)===FALSE)
       {
       file_put_contents('lasthash.txt',$hash."\n",FILE_APPEND);
-      $mail->Body="===================================================================\n";
       $mail->Body.=$project["name"]."\n";
       $mail->Body.=$project["city"]."; ".$project["category"]."\n";
       $mail->Body.=$project["status"]."\n";
+      $mail->Body.=$project["institution"]."\n";
       $mail->Body.="http://www.enviroportal.sk".$project["link"]."\n";
       $mail->Subject.=" ".$project["name"];
+      if ($config["template"])
+         {
+         unset($institutionkey);
+         $templateProcessor = new \PhpOffice\PhpWord\TemplateProcessor($config["template"]);
+         foreach($addresses as $key=>$value)
+            {
+            if (strpos(mb_strtolower($value,'UTF-8'),mb_strtolower($project["institution"],'UTF-8'))!==FALSE)
+               {
+               $institutionkey=$key;
+               break;
+               }
+            }
+         if ($institutionkey)
+            {
+            $addressline=explode("|",$addresses[$institutionkey]);
+            $templateProcessor->setValue('urad',$addressline[0]);
+            $templateProcessor->setValue('urad-ulica',$addressline[2]);
+            $templateProcessor->setValue('urad-mesto',$addressline[4].' '.$addressline[3]);
+            }
+         $templateProcessor->setValue('projekt',$project["name"]);
+         $templateProcessor->setValue('navrhovatel',$project["proponent"]);
+         $templateProcessor->setValue('ico',$project["proponentidnumber"]);
+         $filename=uniqid().".docx";
+         $templateProcessor->saveAs($config["tempdir"].$filename);
+         echo 'File generated';
+         }
+      if ($config["gdrive-clientid"])
+         {
+         $newfile = new Google_Service_Drive_DriveFile();
+         $newfile->setTitle($config["subject"]." ".$project["name"]);
+         $newfile->setMimeType('application/vnd.openxmlformats-officedocument.wordprocessingml.document');
+         $newfile->setWritersCanShare(true);
+         $parent = new Google_Service_Drive_ParentReference();
+         $parent->setId($config["gdrive-folderid"]);
+         $newfile->setParents(array($parent));
+         $additionalparams = array(
+                 'data' => file_get_contents($config["tempdir"].$filename),
+                 'mimeType' => 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+                 'uploadType' => 'multipart',
+                 'convert' => true
+             );
+         $createdfile=$files->insert($newfile,$additionalparams);
+         $createdfileid=$createdfile->getId();
+         $options=array('sendNotificationEmails'=>false);
+         $newpermission=new Google_Service_Drive_Permission();
+         $newpermission->setValue($config["to"]);
+         $newpermission->setType("user");
+         $newpermission->setRole("writer");
+         $service->permissions->insert($createdfileid,$newpermission,$options);
+         if (is_array($config["gdrive-users"]))
+            {
+            foreach($config["gdrive-users"] as $useremail)
+               {
+               $newpermission->setValue($useremail);
+               $newpermission->setType("user");
+               $newpermission->setRole("writer");
+               $service->permissions->insert($createdfileid,$newpermission,$options);
+               }
+            }
+         $mail->Body.="\nEdit: https://docs.google.com/document/d/".$createdfileid."/edit\n";
+         echo ', uploaded to GDrive';
+         }
+      if ($config["template"])
+         {
+         unlink($config["tempdir"].$filename);
+         }
       if(!$mail->send())
          {
-         echo 'Message could not be sent.';
+         echo ', email '.$mail->Subject.' could not be sent<br />';
          echo 'Mailer Error: ' . $mail->ErrorInfo;
          }
       else
          {
-         echo 'Message has been sent';
+         echo ', email '.$mail->Subject.' has been sent';
          }
+      echo '<br />';
       }
    }
 
